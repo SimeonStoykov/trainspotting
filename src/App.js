f03183deb5f5f0b17a73e952e6ffbc33d0a40db9
Simeon Stoykov <simeon.stoikov@gmail.com>
2020-07-27 15:34:16 +0300

    Added less support and made some changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from "react";
-import styles from "./App.css";
+import React, { useState, useEffect } from 'react';
+import styles from './App.less';
 
 function App() {
-  const [trainsInTransit, setTrainsInTransit] = useState("None");
-  const [currentTime, setCurrentTime] = useState("09:00");
+  const [trainsInTransit, setTrainsInTransit] = useState('None');
+  const [currentTime, setCurrentTime] = useState('09:00');
   const [journeys, setJourneys] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/journeys")
+    fetch('http://localhost:3000/journeys')
       .then((res) => res.json())
       .then((data) => setJourneys(data));
   }, []);
@@ -16,40 +16,50 @@ function App() {
     <div className="App">
       <header>
         <h3>Jiminny Trainspotting</h3>
-        <button>Start</button>
-        <section className="header-info">
+        <button hidden>Start</button>
+        <section hidden className="header-info">
           <h4>Trains in transit:&nbsp;</h4>
           <p>{trainsInTransit}</p>
           <h4 id="current-time-title">Current time:&nbsp;</h4>
           <p>{currentTime}</p>
         </section>
       </header>
-      <main className={styles.grid}>
-        <header>
-          <h4>Name</h4>
-          <h4>Route</h4>
-          <h4>Timetable</h4>
-          <h4>Next Station</h4>
-          <h4>Train</h4>
-        </header>
-        <section>
-          {journeys.map((j, i) => (
-            <article key={i}>
-              <div>{j.name}</div>
-              <div>{j.route}</div>
-              <div>
-                {j.timetable.map((t) => (
-                  <>
-                    <div>{t.time}</div>
-                  </>
-                ))}
-              </div>
-              <div>Next station</div>
-              <div>{j.train.name}</div>
-            </article>
-          ))}
-        </section>
-      </main>
+      <table className={styles.grid}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Route</th>
+            <th>Timetable</th>
+            <th hidden id="next-station-title">
+              Next Station
+            </th>
+            <th>Train</th>
+          </tr>
+        </thead>
+        {journeys.map((j, i) => (
+          <tbody key={i}>
+            <tr>
+              <td>{j.name}</td>
+              <td>{j.route}</td>
+              <td>
+                {j.timetable.map((t) => {
+                  const journeyDateTime = new Date(t.time);
+                  console.log(journeyDateTime, t.time);
+                  return (
+                    <p>{`${journeyDateTime.getHours()}:${journeyDateTime.getMinutes()}: ${
+                      t.station
+                    }`}</p>
+                  );
+                })}
+              </td>
+              <td hidden className="next-station-content">
+                Next station
+              </td>
+              <td>{j.train.name}</td>
+            </tr>
+          </tbody>
+        ))}
+      </table>
     </div>
   );
 }
